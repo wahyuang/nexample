a67e6c5b98f981a9001f82d617c54aa6a254d6b0
wahyuang <hello.anggara@gmail.com>
2023-05-26 10:56:09 +0800

    refactor: rename form components

--- src/components/forms/index.tsx
+++ src/components/forms/index.tsx
@@ -1,4 +1,4 @@
-export * from "./ContactForm";
-export * from "./FormErrorMessage";
+export * from "./contact-form";
+export * from "./form-error-message";
 export { default as RegisterForm } from "./register-form";
 export { default as LoginForm } from "./login-form";
